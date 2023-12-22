a7ee12cca0faa29e6a3e0cf6e8de76ab61aadd27
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-22 22:11:13 +0400

    Update slovar.java

--- slovar.java
+++ slovar.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
-public class slovar{
-public static void main(String[] arg){
-Scanner s=new Scanner(System.in);
-System.out.println("Welcome to the dictionary!");
-String a=s.nextLine();
+public class slovar {
+  public static void main(String[] arg) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Welcome to the dictionary!");
+    String a = s.nextLine();
 
+    switch (a) {
+    case "Hello":
+      System.out.println("Privet");
+      break;
 
-switch (a) {
-case "Hello":
-System.out.println("Privet");
-break;
+    case "Bye":
+      System.out.println("Poka");
+      break;
 
-case "Bye":
-System.out.println("Poka");
-break;
+    default:
+      System.out.println("Was not found in the dictionary");
+    }
 
-default:
-System.out.println("Ne bilo naydeno v slovare");
-}
-
-
-}
-
+  }
 
 }
